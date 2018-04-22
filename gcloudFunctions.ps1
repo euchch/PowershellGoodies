3e0099c824e85de7221316d9db946a612d4c275d
Yaakov Yehezkel <koby.yehezkel@doubleverify.com>
2018-04-22 18:23:46 +0300

    Adding new gc functions and some utilities

--- gcloudFunctions.ps1
+++ gcloudFunctions.ps1
@@ -32,12 +32,12 @@ function prodConnect() {
 }
 
 function dvDevConnect() {
-	kubectl.exe config use-context nycd_cluster
+	kubectl config use-context nycd_cluster
 	Set-Variable -Name dvEnvironment -Value "" -Scope Global
 }
 
 function dvProdConnect() {
-	kubectl.exe config use-context nycp_cluster
+	kubectl config use-context nycp_cluster
 	Set-Variable -Name dvEnvironment -Value "" -Scope Global
 }
 
@@ -63,16 +63,16 @@ function gcGetPods {
     )
 
     if ([string]::IsNullOrEmpty($NameSpace)) {
-        kubectl.exe get pods
+        kubectl get pods
         return
     }
 
     if ($Search) {
-        kubectl.exe get pods | Where-Object {$_ -match $NameSpace}
+        kubectl get pods | Where-Object {$_ -match $NameSpace}
         return
     }
 
-    kubectl.exe get pods -n "$dvEnvironment-in-$NameSpace"
+    kubectl get pods -n "$dvEnvironment-in-$NameSpace"
 }
 
 function gcSwitchContext {
@@ -85,7 +85,7 @@ function gcSwitchContext {
 		$nameSpace = "$dvEnvironment-in-$nameSpace"
 	}
 
-    kubectl.exe config current-context | ForEach-Object { kubectl.exe config set-context $_ --namespace=$nameSpace }
+    kubectl config current-context | ForEach-Object { kubectl config set-context $_ --namespace=$nameSpace }
 }
 
 function gcClearJobs {
@@ -95,7 +95,7 @@ function gcClearJobs {
         return
     }
 	
-	kubectl.exe get jobs | Where-Object {$_ -match $JobRegex } | % {Write-Output "kubectl.exe delete job $($_.split(' ')[0])"} | % { (Parallel -Command $_ -Limit 30) }
+	kubectl get jobs | Where-Object {$_ -match $JobRegex } | % {Write-Output "kubectl delete job $($_.split(' ')[0])"} | % { (Parallel -Command $_ -Limit 30) }
 }
 
 function gcToObject {
@@ -108,4 +108,153 @@ function gcToObject {
 	$sb = [scriptblock]::create("$gcCommand")
 	$csvOutput = Invoke-Command $sb | ForEach-Object -Begin {$tempOutput=""} {$line = $_ -replace '\s+',','; $tempOutput += "$line`n"} -end {Write-Output $tempOutput} | ConvertFrom-Csv
 	Write-Output $csvOutput
+}
+
+function gcPodBash {
+	param([Parameter(Mandatory)][string]$PodName)
+
+    if ([string]::IsNullOrEmpty($PodName)) {
+        return
+    }
+	
+	kubectl exec -it $PodName bash
+}
+
+function gcPodCommand {
+	param([Parameter(Mandatory)][string]$PodName,
+	[string]$PodCommand)
+
+    if ([string]::IsNullOrEmpty($PodCommand)) {
+		gcPodBash -PodName $PodName
+        return
+    }
+	
+	Write-Host "Running $PodCommand on $PodName"
+	
+	kubectl exec -t $PodName -- $($PodCommand -split "\s+")
+}
+
+function gcGetRandomPod {
+	param([Parameter(Mandatory)][string]$PodRegex)
+
+    if ([string]::IsNullOrEmpty($PodRegex)) {
+        return
+    }
+	
+	$pods = gcToObject "kubectl.exe get pods" | Where-Object {$_.NAME -match $PodRegex}
+	if ($pods.Count -eq 0) {
+		return
+	}
+	$randomPod = $pods | Get-Random
+	return $randomPod.NAME
+}
+
+function gcGetActivePods {
+	printFromTo -Command "kubectl describe nodes" -PrintFrom "Non-terminated Pods:" -PrintTo "Allocated resources:" -Exception "Name:"
+}
+
+function gcFindResource {
+	param([Parameter(Mandatory)][string]$ResourceRegex)
+
+    if ([string]::IsNullOrEmpty($ResourceRegex)) {
+        return
+    }
+	
+	kubectl.exe get all --all-namespaces | Where-Object {$_ -match $ResourceRegex}
+	if ($pods.Count -eq 0) {
+		return
+	}
+	$randomPod = $pods | Get-Random
+	return $randomPod.NAME
+}
+
+function gcRescale {
+	param([Parameter(Mandatory)][string]$DeploymentRegex,
+		[int]$ReplicasCount
+	)
+
+    if ([string]::IsNullOrEmpty($DeploymentRegex)) {
+        return
+    }
+	
+    if ([string]::IsNullOrEmpty($ReplicasCount)) {
+        return
+    }
+	
+	$deploymentToRescale = gcToObject "kubectl.exe get deployments" | Where-Object { $_.NAME -match $DeploymentRegex} | Select-Object -First 1
+	kubectl.exe scale deployment $deploymentToRescale.NAME --replicas=$ReplicasCount
+	Write-Host "Scaled to to $ReplicasCount replicas"
+}
+
+function gcDataprocConnect {
+    param([Parameter(Mandatory)][string]$Environment,
+    [string]$ClusterName
+    )
+	
+	$dvEnv = ""
+
+    switch ($Environment) {
+        "prime" {$dvEnv = $dvPrimeProject}
+        "dev" {$dvEnv = $dvDevProject}
+        "staging" {$dvEnv = $dvStagingProject}
+        "prod" {$dvEnv = $dvProdProject}
+        default {Write-Output "Please select 'dev', 'staging' or 'prod' environments"}
+    }
+	
+	gcConn $Environment
+	$dpSelectedCluster = gcToObject "gcloud dataproc clusters list" | Where-Object { $_.NAME -match $ClusterName -and $_.STATUS -match 'RUNNING'} | Select-Object -First 1
+	$dvMaster = $dpSelectedCluster.NAME
+	$dvZone = $dpSelectedCluster.ZONE
+	
+	gcToObject "gcloud compute --project $dvEnv ssh --zone $dvZone $($dpSelectedCluster.NAME)-m"
+}
+
+function gcGetActiveSparkApplications {
+    param([Parameter(Mandatory)][string]$Environment,
+    [string]$ClusterName,
+	[switch]$SpecificClusterName
+    )
+	
+	$dvEnv = ""
+
+    switch ($Environment) {
+        "prime" {$dvEnv = $dvPrimeProject}
+        "dev" {$dvEnv = $dvDevProject}
+        "staging" {$dvEnv = $dvStagingProject}
+        "prod" {$dvEnv = $dvProdProject}
+        default {Write-Output "Please select 'dev', 'staging' or 'prod' environments"}
+    }
+	if ($SpecificClusterName) {
+		$dvMaster = $ClusterName
+		$dvZone = $dvDefaultZone
+	} else {
+		gcConn $Environment
+		$dpSelectedCluster = gcToObject "gcloud dataproc clusters list" | Where-Object { $_.NAME -match $ClusterName} | Select-Object -First 1
+		$dvMaster = $dpSelectedCluster.NAME
+		$dvZone = $dpSelectedCluster.ZONE
+	}
+
+	$a = gcToObject "gcloud compute --project $dvEnv ssh --zone $dvZone $dvMaster-m --command 'yarn application --list' | Select -Skip 1" | ft
+	Write-Output $a
+}
+
+function gcInstanceBash {
+	param([Parameter(Mandatory)][string]$PodName)
+
+    if ([string]::IsNullOrEmpty($PodName)) {
+        return
+    }
+	
+	if ([string]::IsNullOrEmpty($dvEnvironment)) {
+        return
+    }
+
+    switch ($dvEnvironment) {
+        "dev" {$dvEnv = $dvDevProject}
+        "staging" {$dvEnv = $dvStagingProject}
+        "prod" {$dvEnv = $dvProdProject}
+        default {return}
+    }
+	
+	gcloud compute --project $dvEnv ssh --zone $dvDefaultZone "$PodName"
 }
--- utilities.ps1
+++ utilities.ps1
@@ -1,3 +1,11 @@
+# Updating active network interface with needed suffixes as W10 keeps on removing them ALL THE TIME when switching wifi/use VPN (windows only)
+If ((Test-Path env:OS) -and ($env:OS -match 'Windows')) {
+    $aryDNSSuffixes = "doubleverify.prod", "doubleverify.corp", "c.staging-1470085110340.internal", "c.prod-1306.internal", "c.dvdev-141815.internal"
+    $obj = Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration | where{-not ($_.IPAddress -match '^172' -or $_.IPAddress -eq $null) -and $_.DHCPEnabled -eq $true}
+    $obj | ForEach-Object {Invoke-CIMMethod -Class win32_networkadapterconfiguration -Name SetDNSSuffixSearchOrder 
+                                            -Arguments @{"DNSDomainSuffixSearchOrder"=$aryDNSSuffixes}}
+}
+
 # Meant to allow us running lots of commands in parallel, using jobs 
 function Parallel {
 	param([int]$Limit,
@@ -36,3 +44,46 @@ function printFromTo {
 													if (-not ($printOutput) -and ($_ -match $Exception)) {Write-Output $_}; 
 												}
 }
+
+function getPSCredentials {
+    param ([string]$User)
+
+    if ([string]::IsNullOrEmpty($User)) {
+        return
+    }
+
+    Write-Host "Please enter password for user $User"
+    $secpasswd = Read-Host -AsSecureString
+    $psCred = New-Object System.Management.Automation.PSCredential ($User, $secpasswd)
+    return $psCred
+}
+
+function installVertica {
+    # Find vertica installation
+    $currentVerticaClient = Get-CimInstance -ClassName Win32_Product | Where-Object { $_.Name -match 'Vertica' }
+    if ($currentVerticaClient.Count) {
+        if ($currentVerticaClient[0].Version -ge '8.1.1') {
+            return
+        }
+    } else {
+        if ($currentVerticaClient.Version -ge '8.1.1') {
+            return
+        }
+    }
+    $currentVerticaClient.Uninstall()
+    Set-Location $env:temp
+    Invoke-WebRequest -Uri 'https://my.vertica.com/client_drivers/8.1.x/8.1.1-8/VerticaSetup-8.1.1-8.exe' -OutFile 'VerticaSetup-8.1.1-8.exe'
+    VerticaSetup-8.1.1-8.exe -q -install
+}
+
+$sb = [scriptblock]::create($function:installVertica)
+$cred = getPSCredentials "domain\user"
+Invoke-Command -ComputerName "tcoltpfit.doubleverify.corp" -Credential $cred -Command $sb
+$cimsession = New-CimSession -Credential $cred -ComputerName "tcoltpfit.doubleverify.corp"
+Get-CimInstance -ClassName Win32_Product -CimSession $cimsession | Where-Object {$_.Name -match 'Vert'}
+
+@("devoltp.doubleverify.corp",
+"nycs-dv-oltp02.doubleverify.prod",
+"nycs-dv-oltp01.doubleverify.prod",
+"10.30.155.157",
+"10.30.155.175") | ForEach-Object {Invoke-Command -ComputerName $_ -Credential $cred -Command $sb -AsJob}
